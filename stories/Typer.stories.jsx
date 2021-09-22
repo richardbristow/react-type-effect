5f9926422db2d326fe562363712b827f0263abc3
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-09-22 15:46:58 +0100

    removed unesscessary imports

--- stories/Typer.stories.jsx
+++ stories/Typer.stories.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import Typer from '../src/components/Typer';
 import TyperBlock from '../src/components/TyperBlock';
 
--- stories/TyperBlock.stories.jsx
+++ stories/TyperBlock.stories.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import TyperBlock from '../src/components/TyperBlock';
 
 export default {
