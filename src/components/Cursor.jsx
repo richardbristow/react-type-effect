c88a7b1a70774027e13f424693fd80768119c1a3
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-09-22 16:10:26 +0100

    changed styled-components import

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,4 +1,4 @@
-import styled, { css, keyframes } from 'styled-components/macro';
+import styled, { css, keyframes } from 'styled-components';
 import PropTypes from 'prop-types';
 
 const cursorBlinkKf = keyframes`
--- src/components/TyperBlock.jsx
+++ src/components/TyperBlock.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components/macro';
+import styled, { css } from 'styled-components';
 
 import Cursor from './Cursor';
 
